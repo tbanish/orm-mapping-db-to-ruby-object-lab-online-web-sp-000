0947483ea2937b54f03cb0af1af049c3ab06bad2
Tom Banish <tombanish@gmail.com>
2020-06-09 21:43:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,6 +19,18 @@ class Student
       self.new_from_db(row)
     end
   end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
   def self.find_by_name(name)
     sql = <<-SQL
@@ -58,4 +70,12 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  
+  
+  
+  
+  
+  
+  
 end
