06a6199889d4a13b18580a8044d6233a180156c4
Tom Banish <tombanish@gmail.com>
2020-06-09 21:52:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,18 @@ class Student
     end.first
   end
   
-
+  def self.all_students_in_grade_x(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
